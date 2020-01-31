d7da9c5fb0383a140dcfc5f751078e28f5fe3082
kaitlynwang <kaitlynwang@172.19.217.119>
2020-01-31 10:25:44 -0800

    upload unit0 from school

---
+++ APCSA 2020/src/unit0/AsciiArt.java
@@ -0,0 +1,28 @@
+package unit0;
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class AsciiArt
+{
+	public static void main ( String[] args )
+	{
+		System.out.println("Kaitlyn \n\n" );
+
+		
+		System.out.println("     ---                    ---      ");
+		System.out.println("                /\\                " );
+		System.out.println("               /  \\               " );
+		System.out.println("              /    \\              " );
+		System.out.println("             [------]             " );
+		System.out.println("                 |");
+		System.out.println("                 |");
+		System.out.println("                / \\");
+		System.out.println("               /   \\");
+		System.out.println("              /     \\");
+	}
+}
---
+++ APCSA 2020/src/unit0/Variables.java
@@ -0,0 +1,59 @@
+package unit0;
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class Variables
+{
+	public static void main ( String[] args )
+	{
+		//define 1 variable of each of the
+		//following data types
+		//byte		short		int 		long
+		//float		double
+		//char      boolean		String
+
+		//integer variables
+		byte byteOne = 127;
+		int shortOne = -32123;
+		int intOne = 90877;
+		int longOne = 999999999;
+
+
+		//decimal variables
+
+
+		//other integer types
+
+
+		//other types
+
+
+		//output your information here
+		System.out.println("/////////////////////////////////");
+		System.out.println("*                               *");
+		System.out.println("*        integer types          *");
+		System.out.println("*                               *");
+		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+		System.out.println("*16 bit - shortOne = "+shortOne+"\t\t*");
+		System.out.println("*32 bit - intOne = "+intOne+"\t\t*");
+		System.out.println("*64 bit - longOne = "+longOne+"\t\t*");
+		
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
